1382c07d56bec2c0ef74d96acd7c589eb90f3649
Andi <63165028+Andi-Tafilaj@users.noreply.github.com>
2020-04-04 16:01:52 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -24,10 +24,10 @@ client.once('ready', () => {
 });
 
 client.on("message", (message) => {
-    if(message.member.hasPermission("ADMINISTRATOR")){  //Who need prefix argument omegalul
+    
         const args = message.content.slice(prefix.length).split(' ');
         const command = args.shift().toLowerCase();
-        if(command === 'cases'){
+        if(command === 'cases' && message.member.hasPermission("ADMINISTRATOR")){
                 if(!message.member.voice.channelID)
                     return message.channel.send("Join a voice channel and enter the command ``!cv cases`` again!");
                 else
@@ -41,7 +41,7 @@ client.on("message", (message) => {
                 }
 
             }
-        else if(command === 'deaths')
+        else if(command === 'deaths' && message.member.hasPermission("ADMINISTRATOR"))
             {
                 if(!message.member.voice.channelID)
                     return message.channel.send("Join a voice channel and enter the command ``!cv deaths`` again!");
@@ -56,7 +56,7 @@ client.on("message", (message) => {
                 }
 
             }
-        else if(command === 'recovered')
+        else if(command === 'recovered' && message.member.hasPermission("ADMINISTRATOR"))
             {
                 if(!message.member.voice.channelID)
                     return message.channel.send("Join a voice channel and enter the command ``!cv recovered`` again!");
@@ -71,7 +71,7 @@ client.on("message", (message) => {
                 }
 
             }
-        else if(command === 'active')
+        else if(command === 'active' && message.member.hasPermission("ADMINISTRATOR"))
             {
                 if(!message.member.voice.channelID)
                     return message.channel.send("Join a voice channel and enter the command ``!cv active`` again!");
@@ -86,7 +86,7 @@ client.on("message", (message) => {
                 }
 
             }
-        else if(command === 'countries')
+        else if(command === 'countries' && message.member.hasPermission("ADMINISTRATOR"))
             {
                 if(!message.member.voice.channelID)
                     return message.channel.send("Join a voice channel and enter the command ``!cv countries`` again!");
@@ -101,25 +101,22 @@ client.on("message", (message) => {
                 }
 
             }
-        else if(command === 'help')
+        else if(command === 'help' && message.member.hasPermission("ADMINISTRATOR"))
         {
             const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Covid-19 Tracker')
                 .setURL('https://discord.js.org/')
-                .setAuthor('shaftAndi#1825', 'https://cdn.discordapp.com/avatars/571412838388727809/52c80f084a56582f9485cd52e43d9e1e.png?size=128')
+                .setAuthor('Bot by shaftAndi#1825', 'https://cdn.discordapp.com/avatars/571412838388727809/52c80f084a56582f9485cd52e43d9e1e.png?size=128')
                 .setDescription('Covid-19 Tracker is an easy to use bot that applies the https://corona.lmao.ninja/all JSON to Voice Channels in order to keep you updated on the latest updates!')
                 .setThumbnail('https://www.dhs.gov/sites/default/files/images/20_0305_opa_coronavirus-micro.jpg')
                 .addFields(
-                    { name: 'Commands', value: '!cv cases\n!cv active\n!cv deaths\n!cv recovered\n!cv countries' }
+                    { name: 'Commands', value: '- !cv cases\n- !cv active\n- !cv deaths\n- !cv recovered\n- !cv countries' }
                 );
             return message.channel.send(helpEmbed);
         }
         else if (message.content.startsWith(`${prefix}`)) {
             message.channel.send("That isn't a valid command, please try again!");
             }
-        }
-        else
-        message.channel.send("Only users with the Administrator permission may use this bot!");
 
     });
